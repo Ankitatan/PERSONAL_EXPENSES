a5c73650e6b1384204fa4a9ba025856523adfbf5
ankita taneja <venusgirlatwork@gmail.com>
2025-10-21 04:19:50 +0530

    Update package installation method and script execution

--- Analyzing Personal Expenses1.py
+++ Analyzing Personal Expenses1.py
@@ -1,4 +1,5 @@
-!pip install pandas faker
+import sys
+!{sys.executable} -m pip install pandas faker
 
 import pandas as pd
 import random
@@ -83,4 +84,3 @@ for month in range(1, 13):  # Loop from January to December
 
 
 python generate_expense_data.py
-!pip install pandas faker streamlit
