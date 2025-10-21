f50e287b5aa0b17d171496f17466b15e0eea0553
ankita taneja <venusgirlatwork@gmail.com>
2025-10-21 06:09:59 +0530

    Install pandas and faker dependencies
    Added installation command for pandas and faker libraries.

--- Analyzing Personal Expenses1.py
+++ Analyzing Personal Expenses1.py
@@ -1,4 +1,5 @@
 # generate_expense_data.py
+pip install pandas faker
 
 import pandas as pd
 import random
@@ -80,3 +81,7 @@ for month in range(1, 13):  # Loop from January to December
     df.to_csv(filename, index=False)
 
     print(f"[✓] {month_name} 2025 - {len(df)} records saved to {filename}")
+
+
+python generate_expense_data.py
+streamlit run "Analyzing Personal Expenses1.py"
